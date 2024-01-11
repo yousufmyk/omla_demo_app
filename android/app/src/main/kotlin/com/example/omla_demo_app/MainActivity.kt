7c6524476f9848c91790f7eaa5d546b97fc5b604
yousufmyk <mohamedyousufdev@gmail.com>
2024-01-11 17:40:58 +0500

    Project Setup

---
+++ android/app/src/main/kotlin/com/example/omla_demo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.omla_demo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const home(),
+    );
+  }
+}
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
